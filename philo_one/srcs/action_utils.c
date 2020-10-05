8c65fb5bbbf6ed9348526cb298d2f77371d12ea2
Irving Micah <imicah@er-b1.kzn.21-school.ru>
2020-10-05 12:04:18 +0300

    fixed check_death func

--- philo_one/srcs/action_utils.c
+++ philo_one/srcs/action_utils.c
@@ -32,13 +32,15 @@ void	put_two_forks(t_philo *philo, int id_fork1, int id_fork2)
 
 void	*check_death(void *thread_data)
 {
-	t_philo		*philo;
+	t_philo				*philo;
 
 	philo = (t_philo*)thread_data;
 	while (get_current_millisecond() - philo->last_lunch_time
 												<= philo->table->time_to_die)
 		usleep(10);
+	pthread_mutex_lock(philo->table->waiter);
 	write_status_philo(philo, " is died...\n");
 	*philo->is_die = 1;
+	pthread_mutex_unlock(philo->table->waiter);
 	return (NULL);
 }
