9660355d80f6b3d35cf8b3607c0762fc91f4832e
Irving Micah <imicah@er-b1.kzn.21-school.ru>
2020-10-05 16:32:55 +0300

    deleted actions utils file in philo_two

--- philo_two/srcs/actions.c
+++ philo_two/srcs/actions.c
@@ -12,6 +12,21 @@
 
 #include "philo_two.h"
 
+void	*check_death(void *thread_data)
+{
+	t_philo				*philo;
+
+	philo = (t_philo*)thread_data;
+	while (get_current_millisecond() - philo->last_lunch_time
+		   <= philo->table->time_to_die)
+		usleep(10);
+	sem_wait(philo->table->sem);
+	write_status_philo(philo, " is died...\n");
+	*philo->is_die = 1;
+	sem_post(philo->table->sem);
+	return (NULL);
+}
+
 void	eating(t_philo *philo)
 {
 	const t_table	*table = philo->table;
